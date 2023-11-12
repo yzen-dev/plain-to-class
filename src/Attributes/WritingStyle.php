26eafefce853c1d7d3b4371b3ec17ad43086c9b6
Volodymyr Hordiienko <vladimir.gordienko@onix-systems.com>
2023-11-12 15:20:02 +0200

    Update WritingStyle.php

--- src/Attributes/WritingStyle.php
+++ src/Attributes/WritingStyle.php
@@ -11,7 +11,7 @@ use Attribute;
  *
  * @psalm-api
  */
-#[Attribute(Attribute::TARGET_PARAMETER)]
+#[Attribute(Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY)]
 final class WritingStyle
 {
     /** Check all possible styles */
